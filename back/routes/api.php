3db041039fab6208cc965b09b31fb3dd86a2ccc9
SavouertPheap <savouert.pheap@student.passerellesnumeriques.org>
2022-09-10 20:31:29 +0700

    #5 create major table because alumni need to know about major info

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BatchController;
+use App\Http\Controllers\MajorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/batch',BatchController::class);
+Route::apiResource('/major',MajorController::class);
