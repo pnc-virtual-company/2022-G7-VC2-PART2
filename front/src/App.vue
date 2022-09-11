5420f2d6dce7d4af76c80d3e581876beb27d8698
mengyer34 <mengyi.yoeng34@gmail.com>
2022-09-11 17:39:53 +0700

    setup new project

---
+++ front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/aba.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline text-pink-700">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
