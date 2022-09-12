fd0a68fe198b8069768e89b252e9204c8c866639
mengyer34 <mengyi.yoeng34@gmail.com>
2022-09-12 09:37:21 +0700

    #3 setup alumni profile vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,37 +1,19 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/aba.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <h1 class="text-3xl font-bold underline text-pink-700">
-    Hello world!
-  </h1>
+  <nav-component></nav-component>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheNavigationBar from './components/nav/TheNavigationBar.vue'
 export default {
   components: {
-    HelloWorld
-  }
+    'nav-component': TheNavigationBar,
+  },
 }
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+body {
+  background: #F6F8F9;
 }
 </style>
