f1102686c469fc12ed5029e59d391a87c07ad381
Sophiem.LOEM <Sophiem.LOEM@pnc.lan>
2022-09-12 09:08:31 +0700

    #2 fix bug

--- back/routes/api.php
+++ back/routes/api.php
@@ -25,9 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 Route::apiResource('/users',UserController::class);
 
 // ===============api for workExperience =============
@@ -44,15 +41,15 @@ Route::post('/company/{id}',[CompanyController::class,'update']);
 Route::get('/position',[PositionController::class,'index']);
 Route::post('/position',[PositionController::class,'store']);
 Route::get('/position/{id}',[PositionController::class,'show']);
-=======
+
 Route::apiResource('/batch',BatchController::class);
 Route::apiResource('/major',MajorController::class);
 Route::apiResource('/alumni',AlumniController::class);
 Route::apiResource('/users',UserController::class);
->>>>>>> 307d5244ef580d9122506c1b4f9d22e3af504a07
-=======
+
+
 Route::apiResource('/batches',BatchController::class);
 Route::apiResource('/majors',MajorController::class);
 Route::apiResource('/alumnis',AlumniController::class);
 Route::apiResource('/users',UserController::class);
->>>>>>> 2c33000f962fb60249a71032d46a95c6e76e66f2
+
