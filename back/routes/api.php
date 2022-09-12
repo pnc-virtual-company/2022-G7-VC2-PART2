5a6a2c45676ec76ce3ed0ea5e9c7a88bc2dc6f07
SavouertPheap <savouert.pheap@student.passerellesnumeriques.org>
2022-09-12 09:48:34 +0700

    #5 update route api to specific route

--- back/routes/api.php
+++ back/routes/api.php
@@ -25,10 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/batches',BatchController::class);
-Route::apiResource('/majors',MajorController::class);
-Route::apiResource('/alumni',AlumniController::class);
-Route::apiResource('/users',UserController::class);
 // ===========Api for Position ============
 Route::get('/positions',[PositionController::class,'index']);
 Route::post('/positions',[PositionController::class,'store']);
@@ -43,17 +39,18 @@ Route::get('/companies',[CompanyController::class,'index']);
 Route::post('/companies',[CompanyController::class,'store']);
 Route::get('/companies/{id}',[CompanyController::class,'show']);
 Route::post('/companies/{id}',[CompanyController::class,'update']);
-
-
-Route::apiResource('/batch',BatchController::class);
-Route::apiResource('/major',MajorController::class);
-Route::apiResource('/alumni',AlumniController::class);
-Route::apiResource('/users',UserController::class);
-
-
-Route::apiResource('/batches',BatchController::class);
-Route::apiResource('/majors',MajorController::class);
-Route::apiResource('/alumnis',AlumniController::class);
+// ============== api for batches ==============
+Route::get('/batches',[BatchController::class,'index']);
+Route::post('/batches',[BatchController::class,'store']);
+Route::post('/batches/{id}',[BatchController::class,'update']);
+// ============== api for majors ==============
+Route::get('/majors',[MajorController::class,'index']);
+Route::post('/majors',[MajorController::class,'store']);
+Route::post('/majors/{id}',[MajorController::class,'update']);
+// ============== api for alumni ==============
+Route::get('/alumni',[AlumniController::class,'index']);
+Route::post('/alumni',[AlumniController::class,'store']);
+Route::post('/alumni/{id}',[AlumniController::class,'update']);
 Route::apiResource('/users',UserController::class);
 
 
